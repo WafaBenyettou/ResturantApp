0c79106f2c580af3ad8f8d50456dce4b7b9bee92
WafaBenyettou <benwafa951@gmail.com>
2023-07-04 16:11:01 +0100

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,11 @@ const Header = () => (
     <div className='app-wrapper-info'>
        <SubHeading title="Chase the new flavour"/>
        <h1 className='app-header-h1'>The key to fine dining</h1>
-       <p className='p-opensans' style={{margin:'2rem 0'}}></p>
+       <p className='p-opensans' style={{margin:'2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
+       <button type="button" className='custom-button'>Explore menu</button>
     </div>
     <div className='app-wrapper-img'>
+    <img src={images.welcome} alt="header_img" />
     </div>
   </div>
 );
