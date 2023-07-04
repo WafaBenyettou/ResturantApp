998eacfae76845ef276555c080de19d81e7b03a7
WafaBenyettou <benwafa951@gmail.com>
2023-07-04 18:29:23 +0100

    special menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './SpecialMenu.css';
+import {  images,data } from "../../constants";
+import { SubHeading, MenuItem } from "../../components";
 
 const SpecialMenu = () => (
   <div>
-    SpecialMenu
+    
   </div>
 );
 
