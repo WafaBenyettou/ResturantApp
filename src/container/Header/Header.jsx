71b05eee663031bda93f086ec203af8e2f1b8027
WafaBenyettou <benwafa951@gmail.com>
2023-07-04 15:54:01 +0100

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import './Header.css';
+import {images} from '../../constants';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app-header app-wrapper section-padding' id='home'>
+    <div className='app-wrapper-info'>
+
+    </div>
+    <div className='app-wrapper-img'>
+    </div>
   </div>
 );
 
