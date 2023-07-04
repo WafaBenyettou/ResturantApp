0896c55f07a4d50d66905443ed3116daf7cc9df4
WafaBenyettou <benwafa951@gmail.com>
2023-07-04 16:07:30 +0100

    header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import './Header.css';
+import {SubHeading} from '../../components'
 import {images} from '../../constants';
 
 const Header = () => (
   <div className='app-header app-wrapper section-padding' id='home'>
     <div className='app-wrapper-info'>
-
+       <SubHeading title="Chase the new flavour"/>
+       <h1 className='app-header-h1'>The key to fine dining</h1>
+       <p className='p-opensans' style={{margin:'2rem 0'}}></p>
     </div>
     <div className='app-wrapper-img'>
     </div>
