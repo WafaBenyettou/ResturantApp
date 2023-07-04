b8ad8bf01f179f7b7527d30ab554bad907de2d1c
WafaBenyettou <benwafa951@gmail.com>
2023-07-04 12:06:35 +0100

    project struct

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React from 'react';
 
-import "./App.css";
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
 
-function App() {
-  return (
-    <>
-      <div>
-        <h1>HELLLLLLLLLLLO</h1>
-      </div>
-    </>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
