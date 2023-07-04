9b79ba1ca385e63af167d5f317db43a4ae3038d2
WafaBenyettou <benwafa951@gmail.com>
2023-07-04 16:20:20 +0100

    about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import "./AboutUs.css";
+import { images } from "../../constants";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app-aboutus app-bg flex-center section-padding" id="about">
+    <div className="app-aboutus-overlay flex-center">
+      <img src={images.G} alt="G-overlay" />
+    </div>
   </div>
 );
 
