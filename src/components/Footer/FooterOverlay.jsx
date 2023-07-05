ddf3215a3e89ccf633d78baa2ed6c97bd2e62382
WafaBenyettou <benwafa951@gmail.com>
2023-07-05 13:12:29 +0100

    footer

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
+  <div className='app-footerOverlay'>
+    <div className='app-footerOverlay-black'></div>
+    <div className='app-footerOverlay-img app-bg'></div> 
   </div>
 );
 
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
+
+import SubHeading from '../SubHeading/SubHeading';
+
+
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app-newsletter'>
+    <div className='app-newsletter-heading'>
+      <SubHeading title='Newsletter' />
+      <h1 className="headtext-cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p-opensans">And never miss latest Updates!</p>
+    </div>
+    <div className='app-newsletter-input flex-center'>
+      <input type='email' placeholder='Enter your email address' />
+      <button type='button' className='custom-button'>Subscribe</button>
+    </div>
   </div>
 );
 
