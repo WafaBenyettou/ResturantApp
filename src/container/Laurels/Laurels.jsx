4dd6aed353b98abbbcdbfcb93134807f574cae94
WafaBenyettou <benwafa951@gmail.com>
2023-07-05 11:28:34 +0100

    awards

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { data, images } from '../../constants';
 import './Laurels.css';
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app-laurels-awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app-laurels-awards-card-content">
+      <p className="p-cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="p-opensans">{subtitle}</p>
+    </div>
+  </div>
+);
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app-bg app-wrapper section-padding' id='awards'>
+    <div className='app-wrapper-info'>
+      <SubHeading title="Awards & recognition" />
+      <h1 className='headtext-cormorant'>Our Laurels</h1>
+
+      <div className='app-laurels-awards'>
+        {data.awards.map((award) => <AwardCard award={award} key={award.title}/>) } 
+      </div>
+    </div>
+    <div className='app-wrapper-img'>
+       <img src={images.laurels} alt='laurels' />
+    </div>
   </div>
 );
 
